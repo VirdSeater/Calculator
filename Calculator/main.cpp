780f93070b1b5560bb9aaead46df0c825ff9886d
VirdSeater <39001974+VirdSeater@users.noreply.github.com>
2019-07-10 18:42:51 +0200

    Upload Calculator
    This is a C++ Calculator lol.

---
+++ Calculator/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char op;
+
+    float num1, num2;
+
+    std::cout << "Please enter the first number!";
+    std::cin >> num1;
+
+    std::cout << "Please enter the second number!";
+    std::cin >> num2;
+
+    std::cout << "Please enter a operator!";
+    std::cin >> op;
+
+    switch (op)
+    {
+        case '+':
+            std::cout << num1 + num2;
+            break;
+        case '-':
+            std::cout << num1 - num2;
+            break;
+        case '*':
+            std::cout << num1 * num2;
+            break;
+        case '/':
+            std::cout << num1 / num2;
+            break;
+        case ':':
+            std::cout << num1 / num2;
+            break;
+        case :
+        default:
+            std::cout << "This operator does not exists!";
+    }
+}
